c0f1359bef79e25fefba9025847d88a2e6612a34
Nino <fazernino@gmail.com>
2025-09-02 12:33:03 +0200

    :sparkles: added login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header';
-import { TodoListComponent } from './features/todos/components/todo-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TodoListComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <app-header></app-header>
-    <main class="container mx-auto p-4"><app-todo-list></app-todo-list></main>
+    <main class="container mx-auto p-4"><router-outlet></router-outlet></main>
   `,
   styles: [],
 })
---
+++ src/app/features/admin/admin.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+// import { LoginComponent } from './components/login/login';
+
+export const ADMIN_ROUTES: Routes = [
+  //   {
+  //     path: 'login',
+  //     component: LoginComponent
+  //   },
+];
