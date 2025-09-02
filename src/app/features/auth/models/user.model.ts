d33d2e872c03052d84e1b76e893c49f7fb02d768
Nino <fazernino@gmail.com>
2025-09-02 11:08:57 +0200

    :tada: added features to add, update or delete tasks and finished config

---
+++ src/app/features/auth/models/user.model.ts
@@ -0,0 +1,16 @@
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+  createdAt: Date;
+}
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+export interface RegisterRequest {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
